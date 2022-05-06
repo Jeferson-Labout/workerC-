0740f964b7df79f309da3d02b7d14e05ad5b2cbc
Jeferson Labout <Jefson1989@gmail.com>
2022-05-06 13:45:49 -0300

    first commit

---
+++ Domain/Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcWorker.Domain
+{
+    public class Calc
+    {
+      
+
+        public string id { get; set; }
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public string status { get; set; }
+        public int resultado { get; set; }
+
+    }
+}
